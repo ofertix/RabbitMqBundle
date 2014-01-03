8ff660af701d369f84ae8e0ae9c8ce817bf2ce03
Miquel Company Rodriguez <miquel.company@ofertix.com>
2014-01-03 16:20:38 +0100

    Add methods to set type and app_id message properties in producer message post.

--- RabbitMq/Producer.php
+++ RabbitMq/Producer.php
@@ -9,6 +9,8 @@ class Producer extends BaseAmqp
 {
     protected $contentType = 'text/plain';
     protected $deliveryMode = 2;
+    protected $appId = null;
+    protected $type = null;
 
     public function setContentType($contentType)
     {
@@ -24,13 +26,27 @@ class Producer extends BaseAmqp
         return $this;
     }
 
+    public function setAppId($appId)
+    {
+        $this->appId = $appId;
+
+        return $this;
+    }
+
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
     public function publish($msgBody, $routingKey = '')
     {
         if ($this->autoSetupFabric) {
             $this->setupFabric();
         }
 
-        $msg = new AMQPMessage($msgBody, array('content_type' => $this->contentType, 'delivery_mode' => $this->deliveryMode));
+        $msg = new AMQPMessage($msgBody, array('content_type' => $this->contentType, 'delivery_mode' => $this->deliveryMode,'app_id' => $this->appId,'type' => $this->appId));
         $this->getChannel()->basic_publish($msg, $this->exchangeOptions['name'], $routingKey);
     }
 }
